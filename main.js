f1803d6fa623dd0db10ac5644d663662e4acb137
sahilmazmudar <sahil.maz97@gmail.com>
2018-12-12 23:57:49 -0800

    Added login to SSC and view finances script

--- main.js
+++ main.js
@@ -2,24 +2,37 @@ const puppeteer = require('puppeteer');
 // dom selectors
 const USERNAME_SELECTOR = '#username';
 const PASSWORD_SELECTOR = '#password';
-const BUTTON_SELECTOR = '#loginForm > div > div > div.td-row > div > div > button';
-
-
+const BUTTON_SELECTOR = '#login > input.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only.btn';
 
 async function login() {
 	const browser = await puppeteer.launch({headless: false});
 	const page = await browser.newPage();
-	await page.goto('target_website');
+	await page.goto('https://ssc.adm.ubc.ca/sscportal/');
 
-	await page.click(USERNAME_SELECTOR);
-	await page.keyboard.type("userId");
+	await page.click("#login > table > tbody > tr > td:nth-child(1) > a");
+	await delay(1000);
 
-	await page.click(PASSWORD_SELECTOR);
-	await page.keyboard.type("password");
+	await page.click('#username');
+	await page.keyboard.type("username");
+
+	await page.click('#password');
+	await page.keyboard.type("pass");
 
 	await page.click(BUTTON_SELECTOR);
 
-	await page.waitForNavigation();
+	await delay(1000);
+
+	await page.click('#UbcMainNav > li:nth-child(3) > a');
+	await delay(2000);
+	const text = page.evaluate(() => document.querySelector('body > div.standard-width > table:nth-child(2) > tbody > tr > td:nth-child(1)').textContent);
+	console.log(text);
+
+}
+
+function delay(time) {
+   return new Promise(function(resolve) { 
+       setTimeout(resolve, time)
+   });
 }
 
 login();
