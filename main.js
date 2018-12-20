c58381cff48abba9ecf7d089d7d3ca6f44aa9426
sahilmazmudar <sahil.maz97@gmail.com>
2018-12-19 21:48:01 -0800

    Finds price DOM element correctly

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const USERNAME_SELECTOR = '#username';
 const PASSWORD_SELECTOR = '#password';
 const BUTTON_SELECTOR = '#login > input.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only.btn';
 const ID = {
-	username: "username",
-	password: "pass"
+	username: "user",
+	password: "password"
 }
 async function login() {
 	const browser = await puppeteer.launch({headless: false});
@@ -25,11 +25,11 @@ async function login() {
 
 	await delay(1000);
 	await page.click('#UbcMainNav > li:nth-child(3) > a');
-	await delay(2000);
-	const text = page.evaluate(() => document.querySelector('body > div.standard-width > table:nth-child(2) > tbody > tr > td:nth-child(1)').textContent);
-	console.log(text);
+	await delay(5000);
+	const element = await page.$("body > div.standard-width > table:nth-child(2) > tbody > tr > td:nth-child(1)");
+	const text = await (await element.getProperty('textContent')).jsonValue();
+    console.log(text);
 }
-
 
 function delay(time) {
    return new Promise(function(resolve) { 
